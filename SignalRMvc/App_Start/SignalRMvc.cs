e09386740b142da59e2ca1b2a01026817039fdd3
Oleksandr Bondarenko <ol.bondarenko@andersenlab.com>
2019-06-05 10:12:22 +0300

    Added SignalR implementation

---
+++ SignalRMvc/App_Start/SignalRMvc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRMvc.App_Start.SignalRMvc))]
+
+namespace SignalRMvc.App_Start
+{
+    public class SignalRMvc
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
